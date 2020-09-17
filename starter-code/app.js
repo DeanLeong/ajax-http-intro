c4eb7d83090c5c24790847dee921143cd49b9e6e
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-09-17 12:38:01 -0600

    updat

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,20 +1,52 @@
 const getData = () => {
-  const url = "http://randomuser.me/api/?results=10"
-  axios.get(url)
+
+  removePerson()
+  const url = "https://randomuser.me/api/?results=10"
+  
+  const headers = {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+
+  axios.get(url, headers)
     .then((res) => {
       // console.log(res) 
       const response = res.data.results
       // console.log(response) 
       response.forEach((person) => {
-        console.log(person.name)
+        // console.log(person.picture.large)
         const peopleDiv = document.querySelector('.people')
         const personDiv = document.createElement('div')
+        personDiv.classList = '.person-div'
         peopleDiv.append(personDiv)
 
+        const name = document.createElement('p')
         const firstName = person.name.first
+        const lastName = person.name.last
+        name.textContent = `${firstName} ${lastName}`
+        personDiv.append(name)
+
+        const img = document.createElement('img')
+        img.src = person.picture.large
+        personDiv.append(img)
 
       })
     })
+    .catch((error) => {
+      console.log(`Error: ${error}`)
+    })
+    .finally(() => {
+      console.log("Look at the beautiful people")
+    })
 }
 
-getData()
+const button = document.querySelector('button')
+button.addEventListener('click', getData)
+
+function removePerson() {
+  const removeDiv = document.querySelector('.people')
+  while(removeDiv.lastChild) {
+    removeDiv.removeChild(removeDiv.lastChild)
+  }
+}
